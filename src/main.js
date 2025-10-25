e0cf895a0df4435cfc511925513d3caf7d43fe9d
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2025-10-25 17:19:23 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles.css' // Import CSS global (jika ada)
 import 'highlight.js/styles/github-dark.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // ... kode Vue lainnya
 // Membuat aplikasi dari komponen App.vue dan menempelkannya ke elemen #app

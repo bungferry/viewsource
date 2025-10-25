2b11f6525cd84b9465ee13b6cb32aa9c60d77936
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2025-10-25 13:12:23 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './style.css' // Import CSS global (jika ada)
 
 // Membuat aplikasi dari komponen App.vue dan menempelkannya ke elemen #app
 createApp(App).mount('#app')

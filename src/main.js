9de087a70e8454dc5cacb388e1337541452ddcc5
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2025-10-25 17:26:19 +0700

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles.css' // Import CSS global (jika ada)
-
-// ... kode Vue lainnya
-// Membuat aplikasi dari komponen App.vue dan menempelkannya ke elemen #app
-createApp(App).mount('#app')
